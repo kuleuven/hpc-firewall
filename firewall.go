f2f4f826951be84a4b2b90af6467c83468affa09
Peter Verraedt <peter@verraedt.be>
2020-05-15 19:28:50 +0200

    Fix javascript
    Signed-off-by: Peter Verraedt <peter@verraedt.be>

--- firewall.go
+++ firewall.go
@@ -14,6 +14,7 @@ import (
 	consul "github.com/hashicorp/consul/api"
 	"github.com/hashicorp/go-hclog"
 	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/oauth2"
 )
 
